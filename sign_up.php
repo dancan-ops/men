52b3762ce8cf7a91b4245d936d0f3dedd6f57a3c
dancan-ops <dannynjugi@gmail.com>
2025-04-03 18:58:48 +0300

    Update sign_up.php
    alright here is what you need to know. upon trying to update the code to connect to a database, I run into errors. xampp on linux has a blocked port being occupied by apache as it was in my case . After going through heaven and back I decieded nginx was the best server for this case. I hope you apache wont cause any conflicts. have a good day

--- sign_up.php
+++ sign_up.php
@@ -1,3 +1,58 @@
+<?php
+
+require('config/connection.php');
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $username = htmlspecialchars($_POST['username']);
+    $password = htmlspecialchars($_POST['password']);
+
+    // Hash the password
+    $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+
+    $errors = [];
+
+    // Username validation (minimum length)
+    if (strlen($username) < 2) {
+        $errors[] = "Username must be at least two characters.";
+    }
+
+    // Password validation (optional)
+    if (strlen($password) < 6) {
+        $errors[] = "Password must be at least six characters.";
+    }
+
+    // If no errors, proceed to insert into the database
+    if (empty($errors)) {
+        // Use prepared statements to avoid SQL injection
+        $query = "INSERT INTO users (username, password) VALUES (?, ?)";
+
+        // Prepare the statement
+        if ($stmt = mysqli_prepare($conn, $query)) {
+            // Bind parameters
+            mysqli_stmt_bind_param($stmt, "ss", $username, $hashed_password);
+            
+            // Execute the statement
+            if (mysqli_stmt_execute($stmt)) {
+                echo "Success";
+
+                header("Location: secondpage.php" );
+
+            } else {
+                echo "Error: " . mysqli_error($conn);
+            }
+            // Close the statement
+            mysqli_stmt_close($stmt);
+        } else {
+            echo "Error: " . mysqli_error($conn);
+        }
+    } else {
+        // Display validation errors
+        foreach ($errors as $error) {
+            echo "<p style='color:red;'>$error</p>";
+        }
+    }
+}
+?>
 
 <!DOCTYPE html>
 <html>
@@ -7,12 +62,12 @@
 	<title>dancan</title>
 
 	<style>
+		body{color: white;}
+
 		form{
 			padding: 10px;
 			padding-left: 40px;
-
 		}
-
 
 		.head{
 			border-radius: 10px 10px 0 0;
@@ -22,9 +77,7 @@
 			height: 15vh;
 			padding: 10px;
 			padding-top: 10px;
-
 		}
-
 
 		input{
 			width: 70vh;
@@ -61,10 +114,10 @@
 <div class="body">
 	<div class="head">
 		<div><a href="login.php">login</a></div>
-		<div><a href="sign_up.php" target="_parent">sign up</a></div>
+		<div>sign up</div>
 	</div>
 
-	<form method="post" action="login.php" target="_parent">
+	<form method="post" action="" target="_parent">
 		<div>
 			<input type="text" name="username" placeholder="username">
 		</div>
@@ -74,12 +127,11 @@
 		</div>
 
 		<div>
-			<input type="submit" name="submit" value="join the vibe" id="submit">
+			<input type="submit" value="join the vibe" id="submit">
 		</div>
 		
 	</form>
 </div>
-
 
 </body>
 </html>
