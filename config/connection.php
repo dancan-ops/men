fdcd2982d28212ada3e7daf0ee854511a517211f
dancan-ops <dannynjugi@gmail.com>
2025-04-03 19:04:07 +0300

    Add files via upload

---
+++ config/connection.php
@@ -0,0 +1,11 @@
+<?php
+
+$conn=mysqli_connect('localhost', 'root','wasakatonge', 'MyDance');
+if (mysqli_connect_errno()) {
+		// if this is true
+		echo 'failed to connect'.mysqli_connect_errno();
+	}else{
+		
+	}
+
+?>
